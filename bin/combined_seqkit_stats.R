0ed36312bc3e4355145b43a07f1a7296992a5b51
Joseph Varberg <jmvarberg@cmh.edu>
2026-07-23 15:27:43 -0500

    Fixes bug overwriting file name in combine stats step to TSV file

--- bin/combined_seqkit_stats.R
+++ bin/combined_seqkit_stats.R
@@ -10,11 +10,10 @@ suppressPackageStartupMessages({
 
 # Read data
 stats <- lapply(args, data.table::fread)
-names(stats) <- basename(args)
 
 print("Number of stats files: ")
 print(length(args))
 
-out <- stats |> dplyr::bind_rows(.id = "file") |> dplyr::arrange(file)
+out <- stats |> dplyr::bind_rows() |> dplyr::arrange(file)
 
 write.csv(out, "./combined_seqkit_stats.csv", row.names = F)
